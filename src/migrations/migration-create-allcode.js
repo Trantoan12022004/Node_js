e7660d809018e1824f22567ce734127fb8b8f56f
Tran_Van_Toan <tranvantoan.work@gmail.com>
2025-02-26 22:09:38 +0700

    2. Tạo database cho dự án

---
+++ src/migrations/migration-create-allcode.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('allcode', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      key: {
+        type: Sequelize.STRING
+      },
+      // password: {
+      //   type: Sequelize.STRING
+      // },
+      type: {
+        type: Sequelize.STRING
+      },
+      value_en: {
+        type: Sequelize.STRING
+      },
+      value_vi: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('allcode');
+  }
+};
--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -1,12 +1,19 @@
 'use strict';
-
+    // positionId: DataTypes.STRING
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        email: 'admin@gmail.com',
+        password: '123456',
+        firstName: 'Tran',
+        lastName: 'Toan',
+        address: 'Thai_Binh',
+        phonenumber: '0363870102',
+        gender: '1',
+        image: null,
+        roleId: 'ADMIN',
+        positionId: null,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
